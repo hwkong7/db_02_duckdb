88f6a6f29b3ec64df29fde9c990f1ebe77057ff3
hwkong7 <hwkong7_@naver.com>
2026-05-14 01:08:26 +0900

    🔨filtering 티커 추가

--- main.py
+++ main.py
@@ -40,11 +40,13 @@ def main(page: ft.Page):
             query = """
                 SELECT * FROM assets 
                 WHERE name ILIKE ?
+                OR ticker ILIKE ?
             """
             search_str = f"%{filter_query}%"
             return con.execute(
                 query,
                 [
+                    search_str,
                     search_str,
                 ],
             ).df()
@@ -103,9 +105,9 @@ def main(page: ft.Page):
     # 필터 입력
     filter_input = ft.Container(
         ft.TextField(
-            label="종목 검색",
+            label="종목 또는 티커 검색",
             prefix_icon=ft.Icons.SEARCH,
-            hint_text="종목명을 입력하세요",
+            hint_text="종목명이나 티커를 입력하세요",
             hint_style=ft.TextStyle(color=ft.Colors.GREY_700),
             expand=True,
             on_change=on_filter_change,
